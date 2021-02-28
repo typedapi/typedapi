f438e9de73880b7bea6a3764c88d45b9c37f62e6
Sergey Sachik <serega3000@gmail.com>
2021-02-28 22:46:06 +0300

    typescript dev dependencies for packages

--- install.sh
+++ install.sh
@@ -1,33 +1,26 @@
-set -e
-
-cd core
-npm install
-npm run build
-
-cd ../server
-npm install
-npm run build
-
-cd ../server-ws
-npm install
-npm run build
-
-cd ../client
-npm install
-npm run build
-
-cd ../parser
-npm install
-npm run build
-
-cd ../client-browser-http
-npm install
-npm run build
-
-cd ../client-browser-ws
-npm install
-npm run build
-
-cd ../redis-signaling
-npm install
-npm run build
+&& set -e \
+&& cd core \
+&& npm install \
+&& npm run build \
+&& cd ../server \
+&& npm install \
+&& npm run build \
+&& cd ../server-ws \
+&& npm install \
+&& npm run build \
+&& cd ../client \
+&& npm install \
+&& npm run build \
+&& cd ../parser \
+&& npm install \
+&& npm run build \
+&& cd ../client-browser-http \
+&& npm install \
+&& npm run build \
+&& cd ../client-browser-ws \
+&& npm install \
+&& npm run build \
+&& cd ../redis-signaling \
+&& npm install \
+&& npm run build \
+|| exit 1
--- test.sh
+++ test.sh
@@ -1,25 +1,17 @@
-set -e
-
-cd core
-npm test
-
-cd ../server
-npm test
-
-cd ../server-ws
-npm test
-
-cd ../client 
-npm test
-
-cd ../parser 
-npm test
-
-cd ../client-browser-http
-npm test
-
-cd ../client-browser-ws
-npm test
-
-cd ../redis-signaling
-npm test
+&& cd core \
+&& npm test \
+&& cd ../server \
+&& npm test \
+&& cd ../server-ws \
+&& npm test \
+&& cd ../client  \
+&& npm test \
+&& cd ../parser  \
+&& npm test \
+&& cd ../client-browser-http \
+&& npm test \
+&& cd ../client-browser-ws \
+&& npm test \
+&& cd ../redis-signaling \
+&& npm test \
+|| exit 1
